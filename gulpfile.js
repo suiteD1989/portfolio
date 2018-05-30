c9f047c989dc94ab70d8bda42453ddb94bc4deac
suiteD1989 <daraghcassidy89@gmail.com>
2018-05-30 13:11:21 +0100

    GA added and cookie consent

--- gulpfile.js
+++ gulpfile.js
@@ -38,7 +38,7 @@ gulp.task('css', function() {
 gulp.task('scripts', function() {
     return gulp.src([
         'node_modules/jquery/dist/jquery.min.js', 
-        'node_modules/parsleyjs/dist/parsley.min.js', 
+        'node_modules/cookieconsent/build/cookieconsent.min.js', 
         'js/custom.js'])
         .pipe(concat('all.js'))
         .pipe(gulp.dest('dist'))
